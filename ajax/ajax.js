c704bd17a094e1f90283c2de6c695ee14e1dcc6c
Curt_H <guardian.curt@outlook.com>
2020-06-12 22:19:49 +0800

    Feat: finish ajax study

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,12 +1,21 @@
-function main() {
+// Build log method
+var log = console.log.bind(console)
+
+var ajax = function () {
+    // Create an xhr object
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://wwww.bilibili.com', true);
+    xhr.open('GET', 'http://localhost:80', true);
+
+
+    xhr.send(null)
     if (xhr.readyState == 4) {
         var content = xhr.responseText;
+        console.log(content);
         document.getElementById('test').innerHTML = content
     }
 }
 
 function run_ajax() {
-    interval = setInterval(main, '1000');
+    console.log('Start');
+    ajax();
 }
