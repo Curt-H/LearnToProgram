a4eee61e418af5f61c67e3c376abb61bc2a6484a
Curt H <guardian.curt@outlook.com>
2021-02-02 20:57:13 +0800

    C++ primer
    Excercise of 1.1.2

--- C++/1.2/a/a/a.cpp
+++ C++/1.2/a/a/a.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 int main() {
-	std::cout << "Enter two numbers:" << std::endl;
+	//std::cout << "Enter two numbers:" << std::endl;
+	//int a = 0, b = 0;
+	//std::cin >> a >> b;
+	//std::cout << "The sum of " << a << " and " << b << " is " << a + b << std::endl;
+
+	// excercise1.3
+	std::cout << "Hello world!" << std::endl;
+
+	// excercise 1.4
 	int a = 0, b = 0;
 	std::cin >> a >> b;
-	std::cout << "The sum of " << a << " and " << b << " is " << a + b << std::endl;
+	std::cout << a * b << std::endl;
+
+	// excecise 1.5
+	std::cout << a;
+	std::cout << b;
+	std::cout << std::endl;
+
+	// excercise 1.6
+	std::cout << "this sum of " << a;
+	std::cout << " and " << b;
+	std::cout << " is " << a + b << std::endl;
+
 	return 0;
 }
